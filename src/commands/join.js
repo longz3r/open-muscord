8da693914dee354bcc8ff5f4567fb73ba30f293f
longz3r <gialongminecraft@gmail.com>
2023-08-05 11:30:09 +0700

    edit join.js

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,4 +1,4 @@
-const { joinVoiceChannel } = require("@discordjs/voice")
+const joinVoiceChannel = require("../functions/joinVoiceChannel")
 
 async function join(interaction) {
     const channel = interaction.options.getChannel("channel")
@@ -7,18 +7,10 @@ async function join(interaction) {
     if (userChannelId == null && channel == undefined) {
         interaction.reply("You need to join or specify a channel for me to join")
     } else if (channel != undefined) {
-        joinVoiceChannel({
-            channelId: channel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
+        joinVoiceChannel(channel, interaction.guildId)
         interaction.reply(`Joined <#${channel.id}>`)
     } else if (channel == undefined) {
-        joinVoiceChannel({
-            channelId: userChannelId,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
+        joinVoiceChannel(userChannelId, interaction.guildId)
         interaction.reply(`Joined <#${userChannelId}>`)
     }
     

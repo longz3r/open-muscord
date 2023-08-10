ce652c36ac79a0b6b4ed28f091a65af9834d1896
longz3r <gialongminecraft@gmail.com>
2023-08-10 10:34:40 +0700

    update it a bit

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,10 +1,20 @@
 const { useQueue } = require("discord-player")
+const verifyQueue = require("../functions/verifyQueue")
 const updateMessage = require("../functions/updateMessage")
 
 async function clear(interaction) {
+    if (!await verifyQueue(interaction.guildId)) {
+        interaction.reply("No connection was found in this server, create one by using **/play** or **/join**")
+        return
+    }
     const queue = useQueue(interaction.guildId)
     queue.clear()
-    // updateMess
+    if (!queue.metadata.nowPlayingMessage) return
+    let currentMessageEmbed = queue.metadata.nowPlayingMessage.embeds[0].data
+    currentMessageEmbed.fields[1].value = "Nothing up next, add some"
+    currentMessageEmbed.fields[4].value = "0"
+
+    updateMessage(queue, currentMessageEmbed)
 
     interaction.reply("**Queue cleared**")
 }
--- src/playerEvents/events/audioTrackRemove.js
+++ src/playerEvents/events/audioTrackRemove.js
@@ -1,7 +1,9 @@
+const updateMessage = require("../../functions/updateMessage")
+
 function audioTrackRemove(queue, track) {
     if (!queue.metadata.nowPlayingMessage) return
     let currentMessageEmbed = queue.metadata.nowPlayingMessage.embeds[0].data
-    currentMessageEmbed.fields[1].value = "\u200b"
+    currentMessageEmbed.fields[1].value = "Nothing up next, add some"
     currentMessageEmbed.fields[4].value = queue.tracks.size
 
     updateMessage(queue, currentMessageEmbed)

b171080abbae91f9fa77c494e18ef9afcf86d0af
Long Zer <gialongminecraft@gmail.com>
2024-02-25 12:10:01 +0700

    add try catch to audioTrackAdd events

--- src/playerEvents/events/audioTracksAdd.js
+++ src/playerEvents/events/audioTracksAdd.js
@@ -2,13 +2,17 @@ const updateMessage = require("../../functions/updateMessage")
 
 function audioTracksAdd(queue, tracks) {
     if (!queue.metadata.nowPlayingMessage) return
-    let currentMessageEmbed = queue.metadata.nowPlayingMessage.embeds[0].data
-    currentMessageEmbed.fields[1].value = `\`\`${queue.tracks.at(0).raw.durationFormatted}\`\` ${queue.tracks.at(0).raw.title}`
-    currentMessageEmbed.fields[4].value = queue.tracks.size
+    try {
+        let currentMessageEmbed = queue.metadata.nowPlayingMessage.embeds[0].data
+        currentMessageEmbed.fields[1].value = `\`\`${queue.tracks.at(0).raw.durationFormatted}\`\` ${queue.tracks.at(0).raw.title}`
+        currentMessageEmbed.fields[4].value = queue.tracks.size
 
-    updateMessage(queue, currentMessageEmbed)
+        updateMessage(queue, currentMessageEmbed)
 
-    queue.metadata.channel.send(`**${tracks.length}** tracks were added to queue`)
+        queue.metadata.channel.send(`**${tracks.length}** tracks were added to queue`)
+    } catch (e) {
+        
+    }
 }
 
 module.exports = audioTracksAdd

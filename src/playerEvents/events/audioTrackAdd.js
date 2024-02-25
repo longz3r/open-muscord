30f4a65c0829a5d28ce171bb018fafc21378c652
Long Zer <gialongminecraft@gmail.com>
2024-02-25 12:18:28 +0700

    remove console.log for debug

--- src/playerEvents/events/audioTrackAdd.js
+++ src/playerEvents/events/audioTrackAdd.js
@@ -1,7 +1,6 @@
 const updateMessage = require("../../functions/updateMessage")
 
 function audioTrackAdd(queue, track) {
-    console.log(queue.metadata.nowPlayingMessage)
     if (!queue.metadata.nowPlayingMessage) return
     try {
         let currentMessageEmbed = queue.metadata.nowPlayingMessage.embeds[0].data
